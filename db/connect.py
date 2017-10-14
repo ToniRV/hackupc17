df845cec8b224352843f67fa560f55b15ab58d02
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-10-14 22:17:57 +0200

    Implement sample queries

--- db/connect.py
+++ db/connect.py
@@ -1,10 +1,31 @@
 #!/bin/python3
-from urllib.parse import quote_plus
+from pprint import pprint
 import pymongo
 
 with open('key.txt') as f:
+#with open('key.txt') as f:
     passcode = f.read()
 
-uri = 'mongodb://%s:%s>@cluster0-shard-00-00-yhrrp.mongodb.net:27017,cluster0-shard-00-01-yhrrp.mongodb.net:27017,cluster0-shard-00-02-yhrrp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin' % ('upc', passcode)
+uri = 'mongodb://%s:%s@cluster0-shard-00-00-yhrrp.mongodb.net:27017,cluster0-shard-00-01-yhrrp.mongodb.net:27017,cluster0-shard-00-02-yhrrp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin' % ('upc', 'hackupc')
 client = pymongo.MongoClient(uri)
-db = client.accounts
+demoset = client.accounts.demo
+
+def get_balance():
+    return demoset.find().sort([('$natural', -1)])[0]
+
+def get_subscriptions():
+    return demoset.find({
+        'subCat': {
+            '$ne': 'noSubCat'
+        }
+    })
+
+def get_subscription_spending():
+    pipe = [{'$match': {'subCat': {'$ne': 'noSubCat'}}},
+            {'$group': {'_id': None,
+                        'sum': {'$sum': "$amount"}}}]
+    return demoset.aggregate(pipeline=pipe)
+
+if __name__=="__main__":
+    for res in get_subscription_spending():
+        pprint(res)
